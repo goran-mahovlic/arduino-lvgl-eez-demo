527b81b2006a2daee56a03057dc542055b797073
Goran Mahovlic <goran.mahovlic@gmail.com>
2022-11-23 23:14:52 +0100

    Adding includes

--- arduino_eez_lvgl_demo/arduino_eez_lvgl_demo.ino
+++ arduino_eez_lvgl_demo/arduino_eez_lvgl_demo.ino
@@ -1,23 +1,24 @@
-#include <lvgl.h>
-#include <TFT_eSPI.h>
-
-#define EEZ_FOR_LVGL
 #define LV_CONF_INCLUDE_SIMPLE
+#define EEZ_FOR_LVGL
 
 #include <eez-framework.h>
+#include <lvgl.h>
+include "lvgl_conf.h"
+
+#include <TFT_eSPI.h>
 #include "ui.h"
 
+extern void ui_init();
+extern void ui_tick();
+
 /*Change to your screen resolution*/
-static const uint16_t screenWidth  = 480;
-static const uint16_t screenHeight = 320;
+static const uint16_t screenWidth  = 320;
+static const uint16_t screenHeight = 240;
 
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf[ screenWidth * 10 ];
 
 TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight); /* TFT instance */
-
-extern void ui_init();
-extern void ui_tick();
 
 /* Display flushing */
 void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
--- arduino_eez_lvgl_demo/ui.h
+++ arduino_eez_lvgl_demo/ui.h
@@ -1,10 +1,9 @@
-#ifndef EEZ_LVGL_UI_GUI_H
-#define EEZ_LVGL_UI_GUI_H
+#ifndef EEZ_LVGL_UI_H
+#define EEZ_LVGL_UI_H
 
-#include <stdint.h>
-#include <eez/flow/lvgl_api.h>
+#include <eez-framework.h>
 
 void ui_init();
 void ui_tick();
 
-#endif // EEZ_LVGL_UI_GUI_H
+#endif // EEZ_LVGL_UI_H
